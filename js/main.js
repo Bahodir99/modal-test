dd1d6f88cb9e27bcbe9da3eedcffea04f5b97ac2
bahodir <bahodirataboyev7@gmail.com>
2023-01-06 11:00:26 +0500

    teste

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var elmodal = document.querySelector('.modal');
-var elclose = document.querySelector('.btn__close');
-var elopen = document.querySelector('.btn__open');
+var elModal = document.querySelector('.modal');
+var elClose = document.querySelector('.btn__close');
+var elOpen = document.querySelector('.btn__open');
 
-elopen.addEventListener('click' , function(){
-    elmodal.classList.add('open')
+elOpen.addEventListener('click' , function(){
+    elModal.classList.add('open')
 })
 
-elclose.addEventListener('click', function(){
-    elmodal.classList.remove('open')
+elClose.addEventListener('click', function(){
+    elModal.classList.remove('open')
 })
